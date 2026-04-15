b00acb67a5749ae024a97889203fb837e3ea9944
Getulio Menegatti <getuliomenegattilara@gmail.com>
2026-04-15 20:18:06 -0300

    Build 9 - limpa cache WebView no Android ao iniciar

--- android/app/src/main/kotlin/com/example/classic_court/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/classic_court/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.example.classic_court
 
+import android.os.Bundle
+import android.webkit.WebStorage
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Limpa todo cache e storage do WebView ao iniciar
+        android.webkit.WebView(this).apply {
+            clearCache(true)
+            clearHistory()
+        }
+        WebStorage.getInstance().deleteAllData()
+    }
+}
