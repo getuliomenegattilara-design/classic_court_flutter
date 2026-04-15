f37e2503a744a76017438a2d8942980125c5aa55
Getulio Menegatti <getuliomenegattilara@gmail.com>
2026-04-15 07:24:36 -0300

    Classic Court Flutter app inicial

---
+++ android/app/src/main/kotlin/com/example/classic_court/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.classic_court
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+ import 'package:flutter/material.dart';                                                                                                                                       
+  import 'package:webview_flutter/webview_flutter.dart';                                                                                                                        
+                                                                                                                                                                                
+  void main() {                                                                                                                                                                     runApp(const ClassicCourtApp());                                                                                                                                            
+  }                                                                                                                                                                                                                                         
+  class ClassicCourtApp extends StatelessWidget {
+    const ClassicCourtApp({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'Classic Court',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2ecc71)),
+          useMaterial3: true,
+        ),
+        home: const ClassicCourtScreen(),
+      );
+    }
+  }
+
+  class ClassicCourtScreen extends StatefulWidget {
+    const ClassicCourtScreen({super.key});
+
+    @override
+    State<ClassicCourtScreen> createState() => _ClassicCourtScreenState();
+  }
+
+  class _ClassicCourtScreenState extends State<ClassicCourtScreen> {
+    late final WebViewController _controller;
+    bool _isLoading = true;
+
+    @override
+    void initState() {
+      super.initState();
+      _controller = WebViewController()
+        ..setJavaScriptMode(JavaScriptMode.unrestricted)
+        ..setNavigationDelegate(NavigationDelegate(
+          onPageFinished: (_) => setState(() => _isLoading = false),
+        ))
+        ..loadRequest(Uri.parse(
+            'https://getuliomenegattilara-design.github.io/ClassicCourt/login.html'));
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        backgroundColor: const Color(0xFF0a0f0d),
+        body: SafeArea(
+          child: Stack(
+            children: [
+              WebViewWidget(controller: _controller),
+              if (_isLoading)
+                const Center(
+                  child: CircularProgressIndicator(
+                    color: Color(0xFF2ecc71),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      );
+    }
+  }
